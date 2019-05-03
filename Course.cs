1175de104f2a8ef682d5400ac49ded7329edb5cd
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2019-05-03 16:58:31 -0400

    initial modelling

---
+++ Course.cs
@@ -0,0 +1,29 @@
+namespace PlutoCFEDB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Courses")]
+    public partial class Course
+    {
+        [Key]
+        public int CourseID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public float Price { get; set; }
+
+        public int? Author_AuthorID { get; set; }
+
+        public int? Tag_TagID { get; set; }
+
+        public virtual Author Author { get; set; }
+
+        public virtual Tag Tag { get; set; }
+    }
+}
---
+++ PlutoContext.cs
@@ -0,0 +1,32 @@
+namespace PlutoCFEDB
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>()
+                .HasMany(e => e.Courses)
+                .WithOptional(e => e.Author)
+                .HasForeignKey(e => e.Author_AuthorID);
+
+            modelBuilder.Entity<Tag>()
+                .HasMany(e => e.Courses)
+                .WithOptional(e => e.Tag)
+                .HasForeignKey(e => e.Tag_TagID);
+        }
+    }
+}
